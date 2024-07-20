6b67112d5b210785b5097c7b84df8257fc280627
Speykious <speykious@gmail.com>
2024-07-20 12:29:23 +0200

    AI-chan v0.2

--- src/commands/remindme.rs
+++ src/commands/remindme.rs
@@ -1,5 +1,6 @@
 use std::{
 	collections::VecDeque,
+	num::ParseIntError,
 	ops::Deref,
 	sync::{Arc, RwLock},
 };
@@ -31,6 +32,16 @@ pub fn register() -> CreateCommand {
 		)
 }
 
+const WRONG: &str = r#"
+
+Valid time formats include:
+- a duration
+  - valid suffixes: `d` `h` `m` `s`, `hr(s)` `min(s)` `sec(s)`, `day(s)` `hour(s)` `minute(s)` `second(s)`
+  - duration format: `<number><suffix> <number><suffix> <number><suffix> ...` (no space between number and suffix)
+  - examples: `1d 3h 10m`, `23day`, `35hrs 4min`, `727secs`
+- a UTC date
+  - valid formats: `YYYY-MM-DD`, `YYYY-MM-DD hh:mm`, `YYYY-MM-DD hh:mm:ss`"#;
+
 pub async fn run(reminders: Arc<RwLock<VecDeque<Reminder>>>, ctx: &Context, command: &CommandInteraction) {
 	let now = date_time_now();
 
@@ -50,19 +61,65 @@ pub async fn run(reminders: Arc<RwLock<VecDeque<Reminder>>>, ctx: &Context, comm
 
 	let timestamp;
 	let mut content;
-	if let Some(date_time) = parse_date_time(&now, time) {
-		let ts = date_time.timestamp();
-		timestamp = Some(ts);
 
-		content = format!("Okie, will remind you on <t:{ts}:F> ~");
-	} else if let Some(date_time) = parse_time_delta(&now, time) {
+	if let Some(date_time) = parse_time_delta(&now, time) {
 		let ts = date_time.timestamp();
 		timestamp = Some(ts);
 
 		content = format!("Okie, will remind you <t:{ts}:R> ~");
 	} else {
-		timestamp = None;
-		content = "Sorry, I couldn't understand the time you sent :c".to_string();
+		match parse_date_time(&now, time) {
+			Ok(date_time) => {
+				let ts = date_time.timestamp();
+				timestamp = Some(ts);
+
+				content = format!("Okie, will remind you on <t:{ts}:F> ~");
+			}
+			Err(e) => {
+				timestamp = None;
+
+				content = match e {
+					ParseDateTimeError::UnrecognizedDateFormat => {
+						"I don't recognize this date format! I only know `YYYY-MM-DD`.".to_string()
+					}
+					ParseDateTimeError::UnrecognizedTimeFormat => {
+						"I don't recognize this time format! I only know `hh:mm` and `hh:mm:ss`.".to_string()
+					}
+					ParseDateTimeError::ParseYear(pie) => {
+						format!("Was that a number for the year? I don't get it :c\n`{pie}`")
+					}
+					ParseDateTimeError::ParseMonth(pie) => {
+						format!("Was that a number for the month? I don't get it :c\n`{pie}`")
+					}
+					ParseDateTimeError::ParseDay(pie) => {
+						format!("Was that a number for the day? I don't get it :c\n`{pie}`")
+					}
+					ParseDateTimeError::InvalidDate => "This date is invalid!".to_string(),
+					ParseDateTimeError::InvalidMonth => {
+						"This month is invalid! There is no more of them after December~".to_string()
+					}
+					ParseDateTimeError::InvalidDay => {
+						"This day is invalid! There are never more than 31 days~".to_string()
+					}
+					ParseDateTimeError::ParseHour(pie) => {
+						format!("Was that a number for the hours? I don't get it :c\n`{pie}`")
+					}
+					ParseDateTimeError::ParseMin(pie) => {
+						format!("Was that a number for the minutes? I don't get it :c\n`{pie}`")
+					}
+					ParseDateTimeError::ParseSec(pie) => {
+						format!("Was that a number for the seconds? I don't get it :c\n`{pie}`")
+					}
+					ParseDateTimeError::InvalidHour => {
+						"This hour is invalid! I don't know how to count after 23, tehe :P".to_string()
+					}
+					ParseDateTimeError::InvalidMin => "This minute is invalid!".to_string(),
+					ParseDateTimeError::InvalidSec => "This second is invalid!".to_string(),
+				};
+
+				content += WRONG;
+			}
+		};
 	};
 
 	'remind_store: {
@@ -102,19 +159,56 @@ pub async fn run(reminders: Arc<RwLock<VecDeque<Reminder>>>, ctx: &Context, comm
 	}
 }
 
-fn parse_date_time(now: &DateTime<Utc>, input: &str) -> Option<DateTime<Utc>> {
+enum ParseDateTimeError {
+	UnrecognizedDateFormat,
+	UnrecognizedTimeFormat,
+
+	ParseYear(ParseIntError),
+	ParseMonth(ParseIntError),
+	ParseDay(ParseIntError),
+
+	InvalidDate,
+
+	InvalidMonth,
+	InvalidDay,
+
+	ParseHour(ParseIntError),
+	ParseMin(ParseIntError),
+	ParseSec(ParseIntError),
+
+	InvalidHour,
+	InvalidMin,
+	InvalidSec,
+}
+
+fn parse_date_time(now: &DateTime<Utc>, input: &str) -> Result<DateTime<Utc>, ParseDateTimeError> {
 	let input = input.trim();
 	let (date_parts, naive_time) = if let Some((date_part, time_part)) = input.split_once(' ') {
 		let time_part = time_part.trim().split(':').collect::<Vec<_>>();
-		let [hour, min, sec] = time_part.as_slice() else {
-			return None;
+
+		let (hour, min, sec) = if let &[h, m, s] = time_part.as_slice() {
+			(h, m, s)
+		} else if let &[h, m] = time_part.as_slice() {
+			(h, m, "0")
+		} else {
+			return Err(ParseDateTimeError::UnrecognizedTimeFormat);
 		};
 
-		let hour: u32 = hour.parse().ok()?;
-		let min: u32 = min.parse().ok()?;
-		let sec: u32 = sec.parse().ok()?;
+		let hour: u32 = hour.parse().map_err(ParseDateTimeError::ParseHour)?;
+		let min: u32 = min.parse().map_err(ParseDateTimeError::ParseMin)?;
+		let sec: u32 = sec.parse().map_err(ParseDateTimeError::ParseSec)?;
 
-		let naive_time = NaiveTime::from_hms_opt(hour, min, sec)?;
+		if hour >= 24 {
+			return Err(ParseDateTimeError::InvalidHour);
+		}
+		if min >= 60 {
+			return Err(ParseDateTimeError::InvalidMin);
+		}
+		if sec >= 60 {
+			return Err(ParseDateTimeError::InvalidSec);
+		}
+
+		let naive_time = NaiveTime::from_hms_opt(hour, min, sec).unwrap();
 
 		let date_parts = date_part.trim().split('-').collect::<Vec<_>>();
 		(date_parts, naive_time)
@@ -126,16 +220,23 @@ fn parse_date_time(now: &DateTime<Utc>, input: &str) -> Option<DateTime<Utc>> {
 	};
 
 	let [year, month, day] = date_parts.as_slice() else {
-		return None;
+		return Err(ParseDateTimeError::UnrecognizedDateFormat);
 	};
 
-	let year: i32 = year.parse().ok()?;
-	let month: u32 = month.parse().ok()?;
-	let day: u32 = day.parse().ok()?;
+	let year: i32 = year.parse().map_err(ParseDateTimeError::ParseYear)?;
+	let month: u32 = month.parse().map_err(ParseDateTimeError::ParseMonth)?;
+	let day: u32 = day.parse().map_err(ParseDateTimeError::ParseDay)?;
 
-	let naive_date = NaiveDate::from_ymd_opt(year, month, day)?;
+	if month > 12 {
+		return Err(ParseDateTimeError::InvalidMonth);
+	}
+	if day > 31 {
+		return Err(ParseDateTimeError::InvalidDay);
+	}
 
-	Some(NaiveDateTime::new(naive_date, naive_time).and_utc())
+	let naive_date = NaiveDate::from_ymd_opt(year, month, day).ok_or(ParseDateTimeError::InvalidDate)?;
+
+	Ok(NaiveDateTime::new(naive_date, naive_time).and_utc())
 }
 
 fn parse_time_delta(now: &DateTime<Utc>, input: &str) -> Option<DateTime<Utc>> {
@@ -152,10 +253,10 @@ fn parse_time_delta(now: &DateTime<Utc>, input: &str) -> Option<DateTime<Utc>> {
 		let n: i64 = digits.parse().ok()?;
 
 		match input_part[n_len..].trim() {
-			"d" => time_delta += TimeDelta::days(n),
-			"h" => time_delta += TimeDelta::hours(n),
-			"m" => time_delta += TimeDelta::minutes(n),
-			"s" => time_delta += TimeDelta::seconds(n),
+			"d" | "day" | "days" => time_delta += TimeDelta::days(n),
+			"h" | "hr" | "hrs" | "hour" | "hours" => time_delta += TimeDelta::hours(n),
+			"m" | "min" | "mins" | "minute" | "minutes" => time_delta += TimeDelta::minutes(n),
+			"s" | "sec" | "secs" | "second" | "seconds" => time_delta += TimeDelta::seconds(n),
 			_ => return None,
 		}
 	}
